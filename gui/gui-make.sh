0d90177adb0c2915d3132c2741d020cf29c4d9ea
TLightSky <wangchao719@gmail.com>
2010-11-11 05:39:48 +0800

    制作cli-make,gui-make

--- gui/gui-make.sh
+++ gui/gui-make.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-python ~/source/PyInstaller/Makespec.py --upx main.py #--onefile 
-python ~/source/PyInstaller/Build.py main.spec
-mv dist/main dist/ok-gui.bin
-cp install.sh dist/install.sh
+#python ~/source/PyInstaller/Makespec.py --upx main.py #--onefile 
+#python ~/source/PyInstaller/Build.py main.spec
+#mv dist/main dist/ok-gui.bin
+#cp install.sh dist/install.sh
+
+#!/bin/bash
+INSTALL_PATH=/usr/local/openkeeper
+cd $(dirname $0)
+SUDOER=sudo
+RELEASE_PATH=openkeeper-gui-1.0
+
+rm -rf $RELEASE_PATH
+mkdir $RELEASE_PATH
+
+cp -rf gui/* $RELEASE_PATH
+rm $RELEASE_PATH/ui/*.pyc
+rm $RELEASE_PATH/ui/*.ui
+rm $RELEASE_PATH/util/*.pyc
+
+echo tar cfz $RELEASE_PATH.tar.gz $RELEASE_PATH
+tar cfz $RELEASE_PATH.tar.gz $RELEASE_PATH
+rm -r $RELEASE_PATH
