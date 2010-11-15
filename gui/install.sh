989c5f72c62a3138fd33e60dde6022971117368f
TLightSky <wangchao719@gmail.com>
2010-11-16 02:17:20 +0800

    判断gui目录是否存在

--- gui/install.sh
+++ gui/install.sh
@@ -3,7 +3,9 @@ INSTALL_PATH=/usr/local/openkeeper
 cd $(dirname $0)
 SUDOER=sudo
 
-$SUDOER mkdir $INSTALL_PATH/gui > /dev/null 2>&1
+if [ ! -d $INSTALL_PATH/gui ] ; then
+	$SUDOER mkdir $INSTALL_PATH/gui #> /dev/null 2>&1
+fi
 $SUDOER cp * $INSTALL_PATH/gui -r && echo 'Install Success'
 $SUDOER ln -sf $INSTALL_PATH/gui/ok-gui.sh /usr/bin/ok-gui
 echo 'Please use ok-gui to start openkeeper gui.'
